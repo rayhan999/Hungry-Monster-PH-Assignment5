c4f831117bddcf579d7e75ad425bc5a659ad67db
rayhan999 <58140227+rayhan999@users.noreply.github.com>
2021-02-07 03:03:46 +0600

    Update script.js

--- script.js
+++ script.js
@@ -21,10 +21,10 @@ const displayFood = foods => {
 
         foods.meals.forEach(food => {
             const foodDiv = document.createElement('div');
-            //countryDiv.className = 'country';
+
             const foodInfo = `
-            <div class="row row-cols-1 row-cols-md-3 g-4 ">
-                <div class="col d-flex justify-content-around">
+            <div class="row row-cols-1 row-cols-md-3 g-4 d-flex flex-nowrap">
+                <div class="col">
                     <div class="card h-100 shadow p-3 bg-white rounded border-0" onclick="displayFoodDetail('${food.idMeal}')" data-bs-toggle="modal" data-bs-target="#exampleModal">
                         <img src="${food.strMealThumb}" class="card-img-top" alt="...">
                         <div class="card-body">
@@ -38,15 +38,19 @@ const displayFood = foods => {
             </div>
             `;
 
-            //     const foodInfo = `
-            //     <div class="card" style="width: 18rem;">
-            //     <img src="..." class="card-img-top" alt="...">
-            //     <div class="card-body">
-            //       <h5 class="card-title">Card title</h5>
-            //       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            //       <a href="#" class="btn btn-primary">Go somewhere</a>
+            // const foodInfo = `
+            //     <div class="row">
+            //         <div class="col-sm-3">
+            //             <div class="card">
+            //                 <img class="card-img-top img-fluid" src="//placehold.it/500x200" alt="Card image cap">
+            //                 <div class="card-block">
+            //                     <h4 class="card-title">Card title</h4>
+            //                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            //                     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            //                 </div>
+            //             </div>
+            //         </div>
             //     </div>
-            //   </div>
             //     `;
             //     const foodInfo = `
             //     <div style="background-color:red" onclick="displayFoodDetail('${food.idMeal}')" data-bs-toggle="modal" data-bs-target="#exampleModal">
