4651ee88f237a4a9dea15e20c855f9b45eb65ed4
rayhan999 <58140227+rayhan999@users.noreply.github.com>
2021-02-07 01:54:22 +0600

    backend done

--- script.js
+++ script.js
@@ -25,8 +25,8 @@ const displayFood = foods => {
             const foodDiv = document.createElement('div');
             //countryDiv.className = 'country';
             const foodInfo = `
-            <div style="background-color:red">
-            <h3 class="country-name">${food.idMeal}</h3>
+            <div style="background-color:red" onclick="displayFoodDetail('${food.idMeal}')">
+            <h3 class="country-name">${food.strMeal}</h3>
             <p>${food.strCategory}</p>
             </div>
         `;
@@ -34,4 +34,29 @@ const displayFood = foods => {
             foodsDiv.appendChild(foodDiv);
         });
     }
+}
+
+const displayFoodDetail = id => {
+    // console.log(id);
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => renderFoodInfo(data.meals[0]));
+}
+
+// const displayFoodDetail = id => {
+//     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => renderCountryInfo(data[0]));
+// }
+
+const renderFoodInfo = food => {
+    const foodDiv = document.getElementById('resultDetails');
+    foodDiv.innerHTML = `
+        <h1>${food.idMeal}</h1>
+        <p>Population: ${food.strMeal}</p>
+        <p>Area: ${food.strCategory}</p>
+        <img src="${food.strMealThumb}">
+    `
 }
